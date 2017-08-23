157ea555b6481646e5f3a7838ed3870bc9c5d1e5
enPiT-P7 <v080ff@yamaguchi-u.ac.jp>
2017-08-23 11:10:58 +0900

    画面レイアウトの作成 データベースとリストビューの連携処理の追加 ・Realmの導入 ・build.gradleの修正 ・データベースの設定処理 ・android:name属性を追加 ・Scheduleクラスを修正 ・ゲッターとセッターの作成 ・Realmオブジェクトの作成 ・Realmベースアダプターの継承 ・処理の実装 ・リストビューにアダプターを設定 データベースへの登録・更新処理の実装 ・画面遷移コードの追加 ・保存処理の追加 ・更新処理の実装 ・ビューの表示制御 ・削除処理の実装

---
+++ MyScheduler/app/src/main/java/com/example/yu_enpit/myscheduler/ScheduleAdapter.java
@@ -0,0 +1,49 @@
+package com.example.yu_enpit.myscheduler;
+
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmBaseAdapter;
+
+/**
+ * Created by enPiT-P7 on 2017/08/05.
+ */
+
+public class ScheduleAdapter extends RealmBaseAdapter<Schedule> {
+
+    private static class ViewHolder{
+        TextView date;
+        TextView title;
+    }
+
+    public ScheduleAdapter(@Nullable OrderedRealmCollection<Schedule> data) {
+        super(data);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(parent.getContext()).inflate(
+                    android.R.layout.simple_list_item_2, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.date = (TextView) convertView.findViewById(android.R.id.text1);
+            viewHolder.title = (TextView) convertView.findViewById(android.R.id.text2);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder)convertView.getTag();
+        }
+
+        Schedule schedule = adapterData.get(position);
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy/MM/dd");
+        String formatDate = sdf.format(schedule.getDate());
+        viewHolder.date.setText(formatDate);
+        viewHolder.title.setText(schedule.getTitle());
+        return convertView;
+    }
+}
